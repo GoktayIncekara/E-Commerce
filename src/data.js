3e4f83adf60d82e0fe51f42abb70c5dccfdf11cb
GoktayIncekara <goktayincekara@gmail.com>
2022-03-05 19:04:27 +0300

    Add initial files and main page

---
+++ src/data.js
@@ -0,0 +1,41 @@
+export const sliderItems = [
+    {
+      id: 1,
+      img: "https://foto.haberler.com/haber/2019/10/20/iztech-roboleague-2019-etkinligi-izmir-12548600_amp.jpg",
+      title: "WINTER SALE",
+      desc: "DON'T COMPROMISE ON STYLE! DRESS WELL IN YOUR COLLAGE (ESPECIALLY IN IZTECH)",
+      bg: "f5fafd",
+    },
+    {
+      id: 2,
+      img: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Iyte_entrance_gate.jpg/500px-Iyte_entrance_gate.jpg",
+      title: "SUMMER COLLECTION",
+      desc: "GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "fcf1ed",
+    },
+    {
+      id: 3,
+      img: "https://cdn.dsmcdn.com/ty76/product/media/images/20210225/21/67018107/146289440/0/0_org_zoom.jpg",
+      title: "YOU ARE IN IZTECH",
+      desc: "GET FLAT 30% OFF JUST FOR STUDYING IZTECH.",
+      bg: "fbf0f4",
+    },
+  ];
+
+  export const categories = [
+    {
+      id: 1,
+      img: "https://images.pexels.com/photos/5886041/pexels-photo-5886041.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+      title: "SHIRT STYLE!",
+    },
+    {
+      id: 2,
+      img: "https://images.pexels.com/photos/2983464/pexels-photo-2983464.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+      title: "LOUNGEWEAR LOVE",
+    },
+    {
+      id: 3,
+      img: "https://images.pexels.com/photos/5480696/pexels-photo-5480696.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      title: "LIGHT JACKETS",
+    },
+  ];
